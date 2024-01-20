babd8287ccca07105a431c9e128d63b3e85f9e14
ValentynaPolkovnikova <137114414+ValentynaPolkovnikova@users.noreply.github.com>
2024-01-20 13:58:24 +0100

    first commit

---
+++ js/task-2.js
@@ -0,0 +1,27 @@
+class Storage {
+    #items = [];
+    
+    constructor(storegeItems) {
+        this.#items = storegeItems;
+    }
+
+    getItems() {
+    return this.#items;
+    }
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+    removeItem(itemToRemove) {
+        const indexToRemove = this.#items.indexOf(itemToRemove);
+        if (indexToRemove !== -1) {
+        this.#items.splice(indexToRemove, 1);
+        }
+    }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
